0860f2f6d820ae0f9d74218e9293d2839fe3f600
Zeky69 <zek.akburak@gmail.com>
2025-01-19 01:14:28 +0100

    add insert

--- database.js
+++ database.js
@@ -87,8 +87,21 @@ function initializeDatabase() {
         ('Pizza', 'Une délicieuse pizza', 12.99, 1000, 45, 60, 'https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg'),
         ('Salade', 'Une délicieuse salade', 8.99, 500, 20, 30, 'https://production-media.gousto.co.uk/cms/mood-image/2236_American-Corn-Salad-With-Ranch-Dressing_0561-1588586763860.jpg'),
         ('Smoothie', 'Un délicieux smoothie', 6.99, 300, 10, 20, 'https://images-na.ssl-images-amazon.com/images/I/71s7s4d6KcL.jpg'),
-        ('Pasta', 'Des pâtes crémeuses', 11.50, 750, 25, 45, 'https://cdn.pixabay.com/photo/2018/07/11/08/23/pasta-3531524_960_720.jpg'),
-        ('Sushi', 'Un assortiment de sushis frais', 15.99, 600, 40, 55, 'https://cdn.pixabay.com/photo/2017/06/29/18/30/sushi-2455981_960_720.jpg');
+        ('Pâte', 'Des pâtes crémeuses', 11.50, 750, 25, 45, 'https://cdn.pixabay.com/photo/2018/07/18/19/12/pasta-3547078_1280.jpg'),
+        ('Sushi', 'Un assortiment de sushis frais', 15.99, 600, 40, 55, 'https://www.dailynews.com/wp-content/uploads/2020/12/SGT-L-DINE-SUSHIROLLS-1204-4-1.jpg'),
+        ('Tacos', 'Des tacos savoureux', 9.99, 700, 30, 40, 'https://cdn.pixabay.com/photo/2023/08/08/08/46/tacos-8176774_1280.jpg'),
+        ('Soupe', 'Une soupe chaude et réconfortante', 7.50, 400, 15, 25, 'https://cdn.pixabay.com/photo/2017/01/24/20/30/soup-2006317_1280.jpg'),
+        ('Steak', 'Un steak juteux', 18.99, 900, 50, 10, 'https://cdn.pixabay.com/photo/2021/05/01/22/01/meat-6222139_1280.jpg'),
+        ('Poulet rôti', 'Un poulet rôti tendre et savoureux', 14.50, 850, 45, 5, 'https://cdn.pixabay.com/photo/2018/08/13/13/37/roasted-chicken-3602956_1280.jpg'),
+        ('Gaufre', 'Une gaufre sucrée et croustillante', 5.99, 400, 6, 50, 'https://cdn.pixabay.com/photo/2019/11/07/13/05/waffle-4608843_1280.jpg'),
+        ('Crêpe', 'Une crêpe fine et moelleuse', 4.99, 350, 8, 45, 'https://cdn.pixabay.com/photo/2019/08/16/16/16/pancakes-4410605_1280.jpg'),
+        ('Curry', 'Un curry épicé et aromatique', 13.50, 600, 30, 50, 'https://cdn.pixabay.com/photo/2022/06/07/20/52/curry-7249247_1280.jpg'),
+        ('Wrap', 'Un wrap frais et léger', 8.50, 450, 20, 35, 'https://cdn.pixabay.com/photo/2021/03/24/08/49/durum-6119590_1280.jpg'),
+        ('Paella', 'Une paella aux fruits de mer', 17.99, 750, 40, 60, 'https://cdn.pixabay.com/photo/2017/06/21/22/44/paella-2428945_1280.jpg'),
+        ('Sandwich', 'Un sandwich garni', 9.50, 600, 25, 45, 'https://cdn.pixabay.com/photo/2017/03/10/13/49/fast-food-2132863_1280.jpg'),
+        ('Donut', 'Un donut sucré au sucre', 2.99, 300, 4, 40, 'https://cdn.pixabay.com/photo/2017/04/04/16/41/cake-2201876_1280.jpg'),
+        ('Ramen', 'Un bol de ramen chaud et savoureux', 13.99, 700, 30, 50, 'https://cdn.pixabay.com/photo/2022/05/10/18/50/ramen-7187810_1280.jpg');
+
     `);
 
     // Insertion dans CLIENT
